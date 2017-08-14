6c50752fc30bfaeac19bc9805cc454c685c18a27
WMXPY <wm@wmpcxpy.com>
2017-08-14 14:09:13 -0500

    initial development frame

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Title from './react/out/title/title';
+import Nav from './react/out/nav/nav';
+import Main from './react/out/main/main';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title>12312312</Title>
+        <Nav>123</Nav>
+        <Main>123</Main>
+      </div>
+    );
+  }
+}
+
+export default App;
