dd22aa3ef7d873a234480476994fd1924c86164c
WMXPY <wm@wmpcxpy.com>
2017-08-22 10:21:55 -0500

    add new styling for icons

--- src/mains/details/rightLane.jsx
+++ src/mains/details/rightLane.jsx
@@ -6,15 +6,31 @@ class RightLane extends Component {
     style = {
         paddingRight: "6px"
     }
+    smallStyle = {
+        fontSize: "16px"
+    }
+    constructor(props) {
+        super(props);
+        this.renderIcon = this.renderIcon.bind(this);
+    }
+
     render() {
         return (
             <span style={this.style}>
-                {this.props.type === "font" ?
-                    <i className={"fa fa-" + this.props.icon + " fa-fw"} style={this.props.style}></i> :
-                    <span style={this.props.style}>—&nbsp;</span>
-                }
+                {this.renderIcon()}
             </span>
         );
+    }
+
+    renderIcon() {
+        switch (this.props.type) {
+            case "font":
+                return <i className={"fa fa-" + this.props.icon + " fa-fw"} style={this.props.style}></i>;
+            case "smallFont":
+                return <i className={"fa fa-" + this.props.icon + " fa-fw"} style={{ ...this.props.style, ...this.smallStyle }}></i>;
+            default:
+                return <span style={this.props.style}>—&nbsp;</span>;
+        }
     }
 }
 
