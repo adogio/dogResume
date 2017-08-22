3d76e3cf28bfb40c69c0f9fe2923eda8cae32c9d
WMXPY <wm@wmpcxpy.com>
2017-08-22 13:25:50 -0500

    zero guide basic platform.

--- src/navbars/zero/zero.jsx
+++ src/navbars/zero/zero.jsx
@@ -3,20 +3,50 @@ import React, {
 } from 'react';
 import strings from '../../strings.json';
 import Language from '../language/language';
+import Button from '../../react/res/button/button';
+import ZeroGuide from './zeroGuide';
 
 class Zero extends Component {
     things;
     constructor(props) {
         super(props);
         this.things = strings.chinese;
+        this.renderPage = this.renderPage.bind(this);
+        this.switchToMainGuide = this.switchToMainGuide.bind(this);
+        this.finish = this.finish.bind(this);
+        this.state = {
+            currentPage: this.props.from
+        }
     }
 
     render() {
         return (
             <div>
-                123
+                {this.renderPage()}
             </div>
         );
+    }
+
+    renderPage() {
+        switch (this.state.currentPage) {
+            case "click":
+                return null;
+            case "language":
+                return <Language next={this.finish} />;
+            case "guide":
+                return <Language next={this.switchToMainGuide} />;
+            case "main":
+                return <ZeroGuide />
+            default:
+        }
+    }
+
+    switchToMainGuide() {
+
+    }
+
+    finish() {
+        window.dog.unPop();
     }
 }
 
